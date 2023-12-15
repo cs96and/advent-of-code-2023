890a0da74124cdb6650ec565e377720282f2c04d
Alan Davies <alan.n.davies@gmail.com>
2023-12-15 11:20:24 +0000

    Added day 15

---
+++ day-15/15.rb
@@ -0,0 +1,32 @@
+def hash_func(str)
+	str.each_char.reduce(0) { |sum, ch| sum = ((sum + ch.ord) * 17) % 256 }
+end
+
+steps = IO.foreach("15.txt", ',', chomp:true).map(&:chomp)
+
+sum = steps.sum { |step| hash_func(step) }
+puts "Part 1: #{sum}"
+
+boxes = []
+steps.each do |step|
+	label, operation, amount = *step.split(/([-=])/)
+	
+	hash_value = hash_func(label)
+
+	box = boxes[hash_value]
+	box = (boxes[hash_value] = {}) if box.nil?
+
+	case operation
+	when '-'
+		box.delete(label)
+	when '='
+		box[label] = amount.to_i
+	end
+end
+
+sum = boxes.each.with_index(1).sum do |box, i|
+	next 0 if box.nil?
+	box.each.with_index(1).sum { |(key, value), j| i * j * value }
+end
+
+puts "Part 2: #{sum}"
