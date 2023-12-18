32ada6243d144e5eeae85941a5749ef6147615ac
Alan Davies <alan.n.davies@gmail.com>
2023-12-18 15:02:44 +0000

    Day 17: Various improvements.  One file for both parts

--- day-17/17.rb
+++ day-17/17.rb
@@ -16,9 +16,11 @@ class String
 end
 
 class PriorityQueue
-	def initialize(cmp: nil)
+	def initialize(&block)
 		@a = []
-		if (@cmp = cmp).nil?
+		if block_given?
+			@cmp = block
+		else
 			@cmp = Proc.new { _1 <=> _2 }
 		end
 	end
@@ -74,7 +76,7 @@ class PriorityQueue
 	def pop_back(*args) = @a.pop(*args)
 end
 
-class Cell
+class Node
 	include Comparable
 
 	attr_reader :y, :x
@@ -93,34 +95,49 @@ class Cell
 		@parent = parent
 	end
 
-	def get_neighbours(max_y, max_x)
-		neighbours = [
-			Cell.new(coord(delta: [1,0]), parent: self),   # Down
-			Cell.new(coord(delta: [0,1]), parent: self),   # Right
-			Cell.new(coord(delta: [-1,0]), parent: self),  # Up
-			Cell.new(coord(delta: [0,-1]), parent: self)   # Left
-		]
+	def in_grid?(grid)
+		@y >= 0 && @y < grid.size && @x >= 0 && @x < grid[0].size
+	end
 
-		neighbours.reject do |n|
-			(@parent && n.coord == @parent.coord) || n.y < 0 || n.y >= max_y || n.x < 0 || n.x >= max_x || n.same_three_times
+	def direction
+		return parent.nil? ? nil : self - @parent
+	end
+
+	def distance_since_last_turn
+		return 0 if @parent.nil?
+		current_dir = direction()
+		node = @parent
+		count = 1
+		while true
+			return count if node.direction != current_dir
+			count += 1
+			node = node.parent
 		end
 	end
 
-	def same_three_times
-		return false if @parent.nil?
-		parent = @parent
-		prev_dir = self - @parent
-		3.times do
-			return false if parent.parent.nil?
-			return false if parent - parent.parent != prev_dir
-			parent = parent.parent
+	def get_neighbours(grid, min_dist, max_dist)
+		dist = distance_since_last_turn()
+		if !@parent.nil? && dist < min_dist
+			dir = self - @parent
+			nxt = Node.new(coord(delta:dir), parent: self)
+			return nxt.in_grid?(grid) ? [nxt] : []
 		end
-		return true
+
+		neighbours = [
+			Node.new(coord(delta: [1,0]), parent: self),   # Down
+			Node.new(coord(delta: [0,1]), parent: self),   # Right
+			Node.new(coord(delta: [-1,0]), parent: self),  # Up
+			Node.new(coord(delta: [0,-1]), parent: self)   # Left
+		]
+
+		neighbours.reject do |n|
+			(n.coord == @parent&.coord) || !n.in_grid?(grid) || n.distance_since_last_turn > max_dist
+		end
 	end
 
 	def distance(rhs)
 		case rhs
-		when Cell
+		when Node
 			(rhs.y - @y).abs + (rhs.x - @x).abs
 		when Array
 			(rhs[0] - @y).abs + (rhs[1] - @x).abs
@@ -164,30 +181,32 @@ def display_grid(grid)
 	puts grid.map { |row| row.map(&:to_s).join }
 end
 
-def a_star(grid, start, goal)
-	open_queue = PriorityQueue.new(cmp: Proc.new{_1.f <=> _2.f})
-	open_queue.insert(Cell.new(start, g: 0, f:0))
+def a_star(grid, start, goal, min_dist, max_dist)
+	# Queue of open nodes, prioritized by f-score
+	open_queue = PriorityQueue.new { _1.f <=> _2.f }
+	open_queue.insert(Node.new(start, g: 0, f:0))
 
 	f_score = Hash.new(Float::INFINITY)
 	
 	until open_queue.empty?
-		cell = open_queue.pop_front
-		#puts "#{cell} - queue: #{open_queue}"
-		if cell.coord == goal
-			cell.display_route(grid)
-			return cell.g
+		node = open_queue.pop_front
+		#puts "#{node} - queue: #{open_queue}"
+		if node.coord == goal
+			next if node.distance_since_last_turn < min_dist
+			node.display_route(grid)
+			return node.g
 		end
  
-		neighbours = cell.get_neighbours(grid.length, grid[0].length)
+		neighbours = node.get_neighbours(grid, min_dist, max_dist)
 		neighbours.each do |n|
-			g = cell.g + grid[n.y][n.x]
+			g = node.g + grid[n.y][n.x]
 			f = g + n.distance(goal)
-			# Need to keep track of the f score of this node with its 3 parents
-			f_key = [n, cell, cell.parent, cell.parent&.parent]
+			# Need to keep track of the f score of this node, its direction, and distance since last turn
+			f_key = [n, n.direction, n.distance_since_last_turn]
 			old_f = f_score[f_key]
 			if f < old_f
 				# This path to a neighbour is better than a previous one
-				# Delete the previous version of the cell from the open queue if one is there
+				# Delete the previous version of the node from the open queue if one is there
 				n.f = old_f
 				open_queue.delete(n)
 
@@ -197,7 +216,6 @@ def a_star(grid, start, goal)
 				open_queue.insert(n)
 			end
 		end
-		#puts "new queue: #{open_queue}"
 	end
 
 	raise "No route found"
@@ -206,5 +224,12 @@ end
 grid = IO.foreach('17.txt', chomp:true).map{ _1.chars.map(&:to_i) }
 puts display_grid(grid)
 
-cost = a_star(grid, [0,0], [grid.length-1, grid[0].length-1])
-puts "Part 1: #{cost}"
+start_node = [0, 0]
+end_node = [grid.length-1, grid[0].length-1]
+
+cost1 = a_star(grid, start_node, end_node, 0, 3)
+puts "Part 1: #{cost1}"
+puts
+
+cost2 = a_star(grid, start_node, end_node, 4, 10)
+puts "Part 2: #{cost2}"
